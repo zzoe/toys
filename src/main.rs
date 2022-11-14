bca297634ac96abea3806b6813625abb917c5304
zoe <zoe211@163.com>
2022-11-14 10:22:35 +0800

    web template

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-mod app;
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     eframe::run_native(
         "Toys",
         eframe::NativeOptions::default(),
-        Box::new(|cc| Box::new(app::App::new(cc))),
+        Box::new(|cc| Box::new(toys::App::new(cc))),
     );
+}
+
+// when compiling to web using trunk.
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Make sure panics are logged using `console.error`.
+    console_error_panic_hook::set_once();
+
+    // Redirect tracing to console.log and friends:
+    tracing_wasm::set_as_global_default();
+
+    let web_options = eframe::WebOptions::default();
+    eframe::start_web(
+        "the_canvas_id", // hardcode it
+        web_options,
+        Box::new(|cc| Box::new(toys::App::new(cc))),
+    )
+    .expect("failed to start eframe");
 }
